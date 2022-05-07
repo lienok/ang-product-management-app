2f4893f0da5c37fa5afe961ca783d4511793399f
weissl <lienok@gmail.com>
2022-05-07 14:52:08 +0200

    filtering on page - data-binding, display nice formatted data with pipes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,31 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule, SkipSelf } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import localeSk from '@angular/common/locales/sk';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+  providers: [
+    {provide: LOCALE_ID, useValue: 'sk' }, 
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'}]
 })
 export class AppModule { }
+
+registerLocaleData(localeSk, 'sk');
