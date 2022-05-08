4ddff9d1ad7430b70a6993b6844a1746b12de845
weissl <lienok@gmail.com>
2022-05-08 13:53:25 +0200

    basic routing and navigation via clickable anchor

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,30 +2,42 @@ import { registerLocaleData } from '@angular/common';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule, SkipSelf } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import localeSk from '@angular/common/locales/sk';
- 
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
+
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent],
 
   providers: [
-    {provide: LOCALE_ID, useValue: 'sk' }, 
-    {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'}]
+    { provide: LOCALE_ID, useValue: 'sk' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }]
 })
 export class AppModule { }
 
