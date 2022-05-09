5a19e98af828872b725c903d6988bedc77740f26
weissl <lienok@gmail.com>
2022-05-09 09:22:55 +0200

    separate product topic to the feature product.module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,38 +1,29 @@
 import { registerLocaleData } from '@angular/common';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule, SkipSelf } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import localeSk from '@angular/common/locales/sk';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import localeSk from '@angular/common/locales/sk';
-
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { StarComponent } from './shared/star.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
-
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent],
 
---
+++ APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+    ])
+  ]
+})
+export class ProductModule { }
